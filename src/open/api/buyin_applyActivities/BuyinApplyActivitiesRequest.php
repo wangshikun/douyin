5bea7bcbcda258065c105ed972e61a3ef113a097
wsk <wangshikunsns@gmail.com>
2022-08-10 09:25:21 +0800

    增加接口

---
+++ src/open/api/buyin_applyActivities/BuyinApplyActivitiesRequest.php
@@ -0,0 +1,48 @@
+<?php
+	namespace ytk\open\api\buyin_applyActivities;
+	use ytk\open\core\GlobalConfig;
+	use ytk\open\core\DoudianOpClient;
+//auto generated code
+class BuyinApplyActivitiesRequest
+{
+
+	private $param;
+
+	private $config;
+
+
+	public function setParam($param)
+	{
+		$this->param = $param;
+	}
+
+	public function getParam()
+	{
+		return $this->param;
+	}
+
+	public function setConfig($config)
+	{
+		$this->config = $config;
+	}
+
+	public function getConfig()
+	{
+		return $this->config;
+	}
+
+	public function getUrlPath()
+	{
+		return "/buyin/applyActivities";
+	}
+
+	public function execute($accessToken)
+	{
+		return DoudianOpClient::getInstance()->request($this, $accessToken);
+	}
+
+	public function __construct()
+	{
+		$this->config = GlobalConfig::getGlobalConfig();
+	}
+}
---
+++ src/open/api/buyin_applyActivities/param/BuyinApplyActivitiesParam.php
@@ -0,0 +1,18 @@
+<?php
+	namespace ytk\open\api\buyin_applyActivities\param;
+
+//auto generated code
+class BuyinApplyActivitiesParam
+{
+
+	public $activity_id;
+
+	public $service_rate;
+
+	public $phone_number;
+
+	public $products;
+
+	public $service_ratio;
+
+}
